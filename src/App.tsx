2eec60577118fe607900ca1035b4997658c97895
Stewart Alsop <stewartalsop@192.168.1.21>
2023-12-31 10:40:37 -0300

    added a mine selector to see if I could center it

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-      <div>
-        hello
+      <div id="mine">
+        mine
       </div>
       <p id="hello">
         "Hello" is a salutation or greeting in the English language. It is first recorded as part of the diction of the New England Algonquians in 1634 in the journal of John Winthrop.
