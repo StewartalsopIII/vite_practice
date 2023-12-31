d062b5d839620f61d9c7c44544ff775b3b6c5ca4
Stewart Alsop <stewartalsop@192.168.1.21>
2023-12-31 10:15:15 -0300

    added a paragraph and made it beautiful

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
       <div>
         hello
       </div>
+      <p id="hello">
+        "Hello" is a salutation or greeting in the English language. It is first recorded as part of the diction of the New England Algonquians in 1634 in the journal of John Winthrop.
+      </p>
     </>
   )
 }
