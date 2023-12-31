f1f0df24d9c950edc9cd0445a39b9401aeacdaa8
Stewart Alsop <stewartalsop@192.168.1.21>
2023-12-31 10:43:55 -0300

    got cursor to add a new container

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import './App.css'
 
 function App() {
-
   return (
-    <>
+    <div className="container">
       <div id="mine">
         mine
       </div>
       <p id="hello">
         "Hello" is a salutation or greeting in the English language. It is first recorded as part of the diction of the New England Algonquians in 1634 in the journal of John Winthrop.
       </p>
-    </>
+    </div>
   )
 }
 
